94302c498079cd8d862eb5f16b60482718cd9fc6
Jgooso <league@WorthyMini-6.attlocal.net>
2017-07-21 15:01:52 -0700

    not

--- src/day3/Magic8Ball.java
+++ src/day3/Magic8Ball.java
@@ -8,18 +8,20 @@ public class Magic8Ball {
 public static void main(String[] args) {
 	int random = new Random().nextInt(5);
 	if (random == 1) {
-	JOptionPane.showMessageDialog(null, "Yes");	
+	JOptionPane.showMessageDialog(null, "Loser");	
 	}
 	if (random == 2) {
-		JOptionPane.showMessageDialog(null, "No");
+		JOptionPane.showMessageDialog(null, "Try asking someone who can give an actual anwser");
 	}
 	if (random == 3) {
-		JOptionPane.showMessageDialog(null, "Maybe you should ask Google");
+		JOptionPane.showMessageDialog(null, "Maybe you should ask Google, like a normal person");
 	}
 	if (random == 4) {
 		JOptionPane.showMessageDialog(null, "Why are you asking me? \nWho told you I was a trustworthy source. \nIf you believe me, you should just give up on life.");
 	}
-		
+		if (random == 0){
+			JOptionPane.showMessageDialog(null, "Whatever your question is, you will never accomplish anything");
+		}
 	
 	
 	
--- src/day5/FlamingNinjaStar.java
+++ src/day5/FlamingNinjaStar.java
@@ -17,31 +17,35 @@ public class FlamingNinjaStar {
 		// *15. Make some adjustments to see what other kinds of shapes you can make.
 
 		// 1. Make a new robot, and set it's pen down.
-
+Robot david= new Robot();
+david.penDown();
+david.hide();
 		// 12. Set the robot speed to 10
-
+david.setSpeed(10);
 		// 13. Make all the code below repeat 25 times
-
+for (int i = 0; i < 50; i++) {
+	
+	david.setRandomPenColor();
 			// 2. Turn the robot 1/8 of a circle
-		
+		david.turn(360/8);
 			// 3. Move the robot 64 pixels
-
+david.move(64);
 			
 			// 4. Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
-			
+			david.turn(-40);
 			// 5. Move the robot the distance in the variable flameSize
-			
+			david.move(flameSize);
 			// 6. Turn the robot 170 degrees
-			
+			david.turn(170);
 			// 7. Move the robot the distance in the variable flameSize (again)
-			
+			david.move(flameSize);
 			// 8. Turn the robot 64 degrees to the right
-			
+			david.turn(360/50);
 			// 9. Move the robot the distance in the variable baseSize
-			
+			david.move(baseSize);
 			// 10. Check that your shape is the same as Figure 1. This is one arm of the ninja star.
 			// 11. Color your ninja star like Figure 2.
-		
+}
 	}
 
 }
